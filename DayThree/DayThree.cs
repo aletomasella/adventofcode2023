7b4b5ae96e94e0d212661a250e352834217ddf34
Alejandro Tomasella <aletomasella09@gmail.com>
2024-01-02 22:46:48 -0300

    feat: Part Two of Day Three Advent of Code

--- DayThree/DayThree.cs
+++ DayThree/DayThree.cs
@@ -43,7 +43,7 @@ public static class DayThree
                         lastIndex = k;
                     }
 
-                    // CHECK IF NUMBER IS VALID
+                    // All possible coordinates for this number
                     var possibleCoordinates = new List<Coordinate>();
 
                     // DIAGONAL FIRST
@@ -66,6 +66,7 @@ public static class DayThree
                     possibleCoordinates.Add(new Coordinate(i - 1, lastIndex));
                     possibleCoordinates.Add(new Coordinate(i + 1, lastIndex));
 
+                    // VERTICAL MIDDLE
                     for (var c = 1; c < number.Length; c++)
                     {
                         possibleCoordinates.Add(new Coordinate(i + 1, firstIndex + c));
@@ -86,10 +87,98 @@ public static class DayThree
         return validNumbers.Sum();
     }
 
-    public static int PartTwo(string[] input)
+    public static long PartTwo(string[] input)
     {
-        return 0;
+        var asteriskNeighbours = new Dictionary<Coordinate, List<int>>();
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            var chars = input[i].ToCharArray();
+
+            for (var j = 0;
+                 j < chars.Length;
+                 j++)
+            {
+                if (chars[j] == '*')
+                {
+                    asteriskNeighbours.Add(new Coordinate(i, j), []);
+                }
+            }
+        }
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            var chars = input[i].ToCharArray();
+
+            for (var j = 0;
+                 j < chars.Length;
+                 j++)
+            {
+                if (char.IsDigit(chars[j]))
+                {
+                    var firstIndex = j;
+                    var lastIndex = j;
+                    var number = chars[j].ToString();
+                    for (int k = j + 1; k < chars.Length; k++)
+                    {
+                        if (!char.IsDigit(chars[k])) break;
+
+                        number += chars[k];
+                        lastIndex = k;
+                    }
+
+                    // All possible coordinates for this number
+                    var possibleCoordinates = new List<Coordinate>();
+
+                    // DIAGONAL FIRST
+                    possibleCoordinates.Add(new Coordinate(i + 1, firstIndex - 1));
+                    possibleCoordinates.Add(new Coordinate(i - 1, firstIndex - 1));
+
+                    // DIAGONAL LAST
+                    possibleCoordinates.Add(new Coordinate(i + 1, lastIndex + 1));
+                    possibleCoordinates.Add(new Coordinate(i - 1, lastIndex + 1));
+
+                    // HORIZONTAL 
+                    possibleCoordinates.Add(new Coordinate(i, firstIndex - 1));
+                    possibleCoordinates.Add(new Coordinate(i, lastIndex + 1));
+
+                    // VERTICAL FIRST
+                    possibleCoordinates.Add(new Coordinate(i + 1, firstIndex));
+                    possibleCoordinates.Add(new Coordinate(i - 1, firstIndex));
+
+                    // VERTICAL LAST
+                    possibleCoordinates.Add(new Coordinate(i - 1, lastIndex));
+                    possibleCoordinates.Add(new Coordinate(i + 1, lastIndex));
+
+                    // VERTICAL MIDDLE
+                    for (var c = 1; c < number.Length; c++)
+                    {
+                        possibleCoordinates.Add(new Coordinate(i + 1, firstIndex + c));
+                        possibleCoordinates.Add(new Coordinate(i - 1, firstIndex + c));
+                    }
+
+                    foreach (var coordinate in possibleCoordinates)
+                    {
+                        if (asteriskNeighbours.ContainsKey(coordinate))
+                        {
+                            asteriskNeighbours[coordinate].Add(int.Parse(number));
+                            break;
+                        }
+                    }
+
+                    j = lastIndex;
+                }
+            }
+        }
+
+
+        var result = asteriskNeighbours
+            .Where(x => x.Value.Count > 1)
+            .Select(x => x.Value.Slice(x.Value.Count - 2, 2));
+
+
+        return result.Sum(x => x[0] * x[1]);
     }
 }
 
-public record Coordinate(int X, int Y);
+public record struct Coordinate(int X, int Y);
--- DayThree/DayThreeTest.cs
+++ DayThree/DayThreeTest.cs
@@ -25,11 +25,16 @@ public static class DayThreeTest
     [Fact]
     public static void PartTwo()
     {
-        var input = new[]
-        {
-            "R8,U5,L5,D3",
-            "U7,R6,D4,L4"
-        };
-        Assert.Equal(30, DayThree.PartTwo(input));
+        var input = @"467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+        Assert.Equal(467835, DayThree.PartTwo(input.Split('\n')));
     }
 }
