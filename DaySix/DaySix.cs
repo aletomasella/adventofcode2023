f1f3d671c1e0c39bdab508b9dbe29dd2d47c5cff
Alejandro Tomasella <aletomasella09@gmail.com>
2024-01-13 12:16:47 -0300

    feat: Part Two Day Six

--- DaySix/DaySix.cs
+++ DaySix/DaySix.cs
@@ -48,8 +48,50 @@ public static class DaySix
     }
 
 
-    public static int PartTwo(string[] input)
+    public static long PartTwo(string[] input)
     {
-        return 0;
+        var result = new List<long>();
+
+        // var input = @"Time:      7  15   30
+        //               Distance:  9  40  200".Split('\n');
+
+        const int aceleration = 1;
+
+        var timeDigits = input[0].Split("Time:")[1].Trim().ToCharArray().Where(Char.IsDigit).Select(x => x.ToString())
+            .ToList();
+
+        var time = long.Parse(String.Join("", timeDigits));
+
+
+        var recordDigits = input[1].Split("Distance:")[1].Trim()
+            .ToCharArray()
+            .Where(Char.IsDigit)
+            .Select(x => x.ToString()).ToList();
+
+        var record = long.Parse(String.Join("", recordDigits));
+
+
+        Console.WriteLine($"Time: {time}");
+
+        Console.WriteLine($"Record: {record}");
+
+
+        var numberOfTimesBeatPerTime = 0;
+
+        long buttonPressTime = 1;
+
+        while (buttonPressTime < time)
+        {
+            var distance = (buttonPressTime * aceleration) * (time - buttonPressTime);
+
+            if (distance > record)
+            {
+                numberOfTimesBeatPerTime++;
+            }
+
+            buttonPressTime++;
+        }
+
+        return numberOfTimesBeatPerTime;
     }
 }
--- DaySix/DaySixTest.cs
+++ DaySix/DaySixTest.cs
@@ -12,4 +12,13 @@ public class DaySixTest
 
         Assert.Equal(288, DaySix.PartOne(input.Split('\n')));
     }
+
+    [Fact]
+    public void PartTwo()
+    {
+        var input = @"Time:      7  15   30
+                      Distance:  9  40  200";
+
+        Assert.Equal(71503, DaySix.PartTwo(input.Split('\n')));
+    }
 }
