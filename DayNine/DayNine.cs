10462fd831f62dbea6e48d6b4258e9e0e594ef84
Alejandro Tomasella <aletomasella09@gmail.com>
2024-01-20 12:32:52 -0300

    feat: Part One Day nine

--- DayNine/DayNine.cs
+++ DayNine/DayNine.cs
@@ -2,9 +2,80 @@ namespace dotnet_aoc.DayNine;
 
 public static class DayNine
 {
-    public static int PartOne(string[] input)
+    public static long PartOne(string[] input)
     {
-        return 0;
+        // var input = @"0 3 6 9 12 15
+        //             1 3 6 10 15 21
+        //             10 13 16 21 30 45".Split('\n');
+
+        long result = 0;
+
+
+        foreach (var line in input)
+        {
+            var sequence = line.Trim().Split(' ').Select(int.Parse).ToArray();
+
+            var extrapolatedNumber = GetExtrapolatedNumber(sequence);
+
+            result += extrapolatedNumber;
+        }
+
+
+        return result;
+    }
+
+    private static long GetExtrapolatedNumber(int[] sequence)
+    {
+        var matrix = new int[sequence.Length + 1, sequence.Length + 1];
+
+        var allZeros = false;
+
+        for (var i = 0; i < sequence.Length; i++)
+        {
+            // FIRST ROW OF MATRIX
+            matrix[0, i] = sequence[i];
+        }
+
+        var row = 0;
+
+        while (!allZeros)
+        {
+            allZeros = true;
+
+            for (var i = 0; i < sequence.Length - 1 - row; i++)
+            {
+                var leftValue = matrix[row, i];
+                var rightValue = matrix[row, i + 1];
+                var difference = rightValue - leftValue;
+
+
+                matrix[row + 1, i] = difference;
+
+                if (difference != 0)
+                {
+                    allZeros = false;
+                }
+            }
+
+            row++;
+        }
+
+
+        // GOING UP ROWS
+        for (var r = row; r > 0; r--)
+        {
+            var lastIdx = sequence.Length - r;
+            var difference = matrix[r, lastIdx];
+            var valueAbove = matrix[r - 1, lastIdx];
+
+            matrix[r - 1, lastIdx + 1] = valueAbove + difference;
+        }
+
+        var result = matrix[0, sequence.Length];
+
+        // Console.WriteLine($"RESULT: {result}");
+
+        return matrix[0, sequence.Length];
     }
 
     public static int PartTwo(string[] input)
--- DayNine/DayNineTest.cs
+++ DayNine/DayNineTest.cs
@@ -7,24 +7,20 @@ public class DayNineTest
     [Fact]
     public void PartOne()
     {
-        var input = @"10  13  16  21  30  45  68
-   3   3   5   9  15  23
-     0   2   4   6   8
-       2   2   2   2
-         0   0   0";
+        var input = @"0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
 
-        Assert.Equal(127, DayNine.PartOne(input.Split('\n')));
+        Assert.Equal(114, DayNine.PartOne(input.Split('\n')));
     }
 
 
     [Fact]
     public void PartTwo()
     {
-        var input = @"10  13  16  21  30  45  68
-   3   3   5   9  15  23
-     0   2   4   6   8
-       2   2   2   2
-         0   0   0";
+        var input = @"0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
 
         Assert.Equal(62, DayNine.PartTwo(input.Split('\n')));
     }
